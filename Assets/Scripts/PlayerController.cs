3dfd3020910b69fd04ccc26a319ac6ecbe581d0f
ahmadadi1415 <adipras606@gmail.com>
2024-06-19 09:31:31 +0700

    [feat] add player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private int speed = 350;
+    private CharacterController _controller;
+    private Vector3 _moveDirection;
+
+    private void Awake() {
+        _controller = GetComponent<CharacterController>();    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        _controller.Move(_moveDirection * speed / 10 * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("Throw Food");
+        }
     }
 }
