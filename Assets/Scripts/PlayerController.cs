6535d1d164447bd8314b847a04007bef0e0cbdf3
ahmadadi1415 <adipras606@gmail.com>
2024-06-19 09:41:11 +0700

    [feat] setup character animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private int speed = 350;
-    private CharacterController _controller;
     private Vector3 _moveDirection;
-
+    private CharacterController _controller;
+    [SerializeField] private Animator _animator;
+    
     private void Awake() {
         _controller = GetComponent<CharacterController>();    
     }
@@ -14,8 +15,12 @@ public class PlayerController : MonoBehaviour
         _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
         _controller.Move(_moveDirection * speed / 10 * Time.deltaTime);
 
+        _animator.SetBool("isMoving", _moveDirection != Vector3.zero);
+        _animator.SetFloat("horizontalMovement", _moveDirection.x);
+
         if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) {
             Debug.Log("Throw Food");
+            _animator.SetTrigger("throw");
         }
     }
 }
